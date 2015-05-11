d461bd83e9121ec7c4fa738cc4bb2212ac415c8b
TonyAdo <coolhzb@163.com>
2015-05-11 14:47:47 +0800

    add the DatumWriter ...

---
+++ baiji-core/src/main/java/com/xqbase/baiji/specific/SpecificDatumWriter.java
@@ -0,0 +1,11 @@
+package com.xqbase.baiji.specific;
+
+import com.xqbase.baiji.generic.GenericDatumWriter;
+
+/**
+ * {@link com.xqbase.baiji.io.DatumWriter} for generated Java classes.
+ *
+ * @author Tony He
+ */
+public class SpecificDatumWriter<T> extends GenericDatumWriter<T> {
+}
