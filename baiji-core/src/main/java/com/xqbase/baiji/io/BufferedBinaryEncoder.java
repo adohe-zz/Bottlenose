49692313dbd16292fe23bb3eb681405a960d9573
TonyAdo <coolhzb@163.com>
2015-05-13 19:46:44 +0800

    add the Binary&JSON serializer...

--- baiji-core/src/main/java/com/xqbase/baiji/io/BufferedBinaryEncoder.java
+++ baiji-core/src/main/java/com/xqbase/baiji/io/BufferedBinaryEncoder.java
@@ -1,102 +1,120 @@
 package com.xqbase.baiji.io;
 
-import com.xqbase.baiji.util.Utf8;
-
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.WritableByteChannel;
 import java.util.Calendar;
 
 /**
  * A {@link Encoder} for Baiji's binary encoding.
  * <p/>
+ * This implementation buffers output to enhance performance.
+ * Output may not appear on the underlying output until flush() is called.
  *
  * @see Encoder
  * @see Decoder
  *
  * @author Tony He
  */
-public class BufferedBinaryEncoder implements Encoder {
+public class BufferedBinaryEncoder extends BinaryEncoder {
 
-    private static final int ONE_MINUTE = 1000 * 60;
+    private byte[] buf;
+    private int pos;
+    private ByteSink sink;
+    private int bulkLimit;
 
-    private final OutputStream _stream;
-
-    public BufferedBinaryEncoder() {
-        this(null);
+    public BufferedBinaryEncoder(OutputStream out, int bufferSize) {
+        configure(out, bufferSize);
     }
 
-    public BufferedBinaryEncoder(OutputStream stream) {
-        _stream = stream;
+    BufferedBinaryEncoder configure(OutputStream out, int bufferSize) {
+        if (null == out) {
+            throw new NullPointerException("OutputStream cannot be null");
+        }
+
+        if (this.sink != null) {
+
+        }
+
+        this.sink = new OutputStreamSink(out);
+        pos = 0;
+        if (null == buf || buf.length != bufferSize) {
+            buf = new byte[bufferSize];
+        }
+        bulkLimit = buf.length >>> 1;
+        if (bulkLimit > 512) {
+            bulkLimit = 512;
+        }
+        return this;
+    }
+
+    /**
+     * Flushes the internal buffer to the underlying output.
+     * Does not flush the underlying output.
+     */
+    private void flushBuffer() throws IOException {
+        if (pos > 0) {
+            sink.innerWrite(buf, 0, pos);
+            pos = 0;
+        }
+    }
+
+    /**
+     * Ensures that the buffer has at least num bytes free to write to between its
+     * current position and the end. This will not expand the buffer larger than
+     * its current size, for writes larger than or near to the size of the buffer,
+     * we flush the buffer and write directly to the output, bypassing the buffer.
+     * @param num number of bytes free
+     * @throws IOException
+     */
+    private void ensureBounds(int num) throws IOException {
+        int remaining = buf.length - pos;
+        if (remaining < num) {
+            flushBuffer();
+        }
     }
 
     @Override
-    public void writeNull() throws IOException {
+    protected void writeZero() throws IOException {
+
+    }
+
+    @Override
+    public int bytesBuffered() {
+        return 0;
     }
 
     @Override
     public void writeBoolean(boolean b) throws IOException {
-        doWriteByte((byte) (b ? 1 : 0));
-    }
-
-    @Override
-    public void writeInt(int value) throws IOException {
-        writeLong(value);
-    }
-
-    public void writeLong(long value) throws IOException {
-        long n = (value << 1) ^ (value >> 63);
-        while ((n & ~0x7FL) != 0) {
-            doWriteByte((byte) ((n | 0x80) & 0xFF));
-            n >>>= 7;
+        if (buf.length == pos) {
+            flushBuffer();
         }
-        doWriteByte((byte) n);
-    }
-
-    public void writeFloat(float value) throws IOException {
-        int bits = Float.floatToRawIntBits(value);
-        doWriteByte((byte) ((bits) & 0xFF));
-        doWriteByte((byte) ((bits >> 8) & 0xFF));
-        doWriteByte((byte) ((bits >> 16) & 0xFF));
-        doWriteByte((byte) ((bits >> 24) & 0xFF));
     }
 
     @Override
-    public void writeDouble(double value) throws IOException {
-        long bits = Double.doubleToLongBits(value);
-        doWriteByte((byte) ((bits) & 0xFF));
-        doWriteByte((byte) ((bits >> 8) & 0xFF));
-        doWriteByte((byte) ((bits >> 16) & 0xFF));
-        doWriteByte((byte) ((bits >> 24) & 0xFF));
-        doWriteByte((byte) ((bits >> 32) & 0xFF));
-        doWriteByte((byte) ((bits >> 40) & 0xFF));
-        doWriteByte((byte) ((bits >> 48) & 0xFF));
-        doWriteByte((byte) ((bits >> 56) & 0xFF));
+    public void writeInt(int n) throws IOException {
+        ensureBounds(5);
+        pos += BinaryData.encodeInt(n, buf, pos);
     }
 
     @Override
-    public void writeString(Utf8 str) throws IOException {
-
+    public void writeLong(long n) throws IOException {
+        ensureBounds(10);
+        pos += BinaryData.encodeLong(n, buf, pos);
     }
 
     @Override
-    public void writeString(String str) throws IOException {
-
+    public void writeFloat(float f) throws IOException {
+        ensureBounds(4);
+        pos += BinaryData.encodeFloat(f, buf, pos);
     }
 
     @Override
-    public void writeString(CharSequence charSequence) throws IOException {
-
-    }
-
-    @Override
-    public void writeBytes(byte[] bytes, int start, int len) throws IOException {
-
-    }
-
-    @Override
-    public void writeBytes(ByteBuffer bytes) throws IOException {
-
+    public void writeDouble(double d) throws IOException {
+        ensureBounds(8);
+        pos += BinaryData.encodeDouble(d, buf, pos);
     }
 
     @Override
@@ -106,57 +124,57 @@ public class BufferedBinaryEncoder implements Encoder {
 
     @Override
     public void writeDatetime(Calendar date) throws IOException {
-        int offset = date.getTimeZone().getOffset(date.getTimeInMillis());
-        int minutes = offset/ONE_MINUTE;
-        writeLong(date.getTimeInMillis());
-        writeLong(minutes);
-    }
 
-    @Override
-    public void writeEnum(int value) throws IOException {
-        writeLong(value);
-    }
-
-    @Override
-    public void startItem() throws IOException {
-    }
-
-    @Override
-    public void setItemCount(long value) throws IOException {
-        if (value > 0) {
-            writeLong(value);
-        }
-    }
-
-    public void writeArrayStart() throws IOException {
-    }
-
-    public void writeArrayEnd() throws IOException {
-        writeLong(0);
-    }
-
-    public void writeMapStart() throws IOException {
-    }
-
-    public void writeMapEnd() throws IOException {
-        writeLong(0);
-    }
-
-    public void writeUnionIndex(int value) throws IOException {
-        writeLong(value);
-    }
-
-    private void doWriteBytes(byte[] bytes) throws IOException {
-        _stream.write(bytes, 0, bytes.length);
-    }
-
-    private void doWriteByte(byte b) throws IOException {
-        _stream.write(b);
     }
 
     @Override
     public void flush() throws IOException {
-        _stream.flush();
+        flushBuffer();
+        sink.innerFlush();
+    }
+
+    /**
+     * ByteSink abstracts the destination of written data from the core workings
+     * of BinaryEncoder.
+     */
+    private abstract static class ByteSink {
+
+        protected ByteSink() {}
+
+        /** Write data from bytes, starting at off, for len bytes. */
+        protected abstract void innerWrite(byte[] bytes, int off, int len) throws IOException;
+
+        protected abstract void innerWrite(ByteBuffer buffer) throws IOException;
+
+        /** Flush the underlying output. */
+        protected abstract void innerFlush() throws IOException;
+    }
+
+    static class OutputStreamSink extends ByteSink {
+
+        private final OutputStream out;
+        private final WritableByteChannel channel;
+
+        private OutputStreamSink(OutputStream out) {
+            super();
+            this.out = out;
+            this.channel = Channels.newChannel(out);
+        }
+
+        @Override
+        protected void innerWrite(byte[] bytes, int off, int len) throws IOException {
+            out.write(bytes, off, len);
+        }
+
+        @Override
+        protected void innerWrite(ByteBuffer buffer) throws IOException {
+            channel.write(buffer);
+        }
+
+        @Override
+        protected void innerFlush() throws IOException {
+            out.flush();
+        }
     }
 }
 
