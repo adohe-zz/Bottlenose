188d8dd0f79869d6e2e1aacebd0aad457ded93e5
TonyAdo <coolhzb@163.com>
2015-05-15 11:39:32 +0800

    add the SpecificDatumReader...

---
+++ baiji-core/src/main/java/com/xqbase/baiji/specific/SpecificDatumReader.java
@@ -0,0 +1,11 @@
+package com.xqbase.baiji.specific;
+
+import com.xqbase.baiji.generic.GenericDatumReader;
+
+/**
+ * {@link com.xqbase.baiji.io.DatumReader} for generated Java objects.
+ *
+ * @author Tony He
+ */
+public class SpecificDatumReader<T> extends GenericDatumReader<T> {
+}
