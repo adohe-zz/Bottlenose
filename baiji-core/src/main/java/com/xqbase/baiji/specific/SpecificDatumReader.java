3fb7d8367ff7c3fda762a85712f94123668ff0ea
TonyAdo <coolhzb@163.com>
2015-05-18 10:49:42 +0800

    remove the old PreResolvingDatumReader and try to use the parser approach...

--- baiji-core/src/main/java/com/xqbase/baiji/specific/SpecificDatumReader.java
+++ baiji-core/src/main/java/com/xqbase/baiji/specific/SpecificDatumReader.java
@@ -1,11 +1,9 @@
 package com.xqbase.baiji.specific;
 
-import com.xqbase.baiji.generic.GenericDatumReader;
-
 /**
  * {@link com.xqbase.baiji.io.DatumReader} for generated Java objects.
  *
  * @author Tony He
  */
-public class SpecificDatumReader<T> extends GenericDatumReader<T> {
+public class SpecificDatumReader<T> {
 }
