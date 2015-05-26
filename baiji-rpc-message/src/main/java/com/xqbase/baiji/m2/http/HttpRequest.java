a9526081c3c5fa0e2b9a5200e2e48dd7911d54da
TonyAdo <coolhzb@163.com>
2015-05-26 17:30:22 +0800

    add the http/tcp request/response layer...

---
+++ baiji-rpc-message/src/main/java/com/xqbase/baiji/m2/http/HttpRequest.java
@@ -0,0 +1,21 @@
+package com.xqbase.baiji.m2.http;
+
+import com.xqbase.baiji.m2.Request;
+
+import java.net.URI;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by tonyhe on 15-5-26.
+ */
+public class HttpRequest implements Request {
+    @Override
+    public URI getUri() {
+        return null;
+    }
+
+    @Override
+    public ByteBuffer getEntity() {
+        return null;
+    }
+}
---
+++ baiji-rpc-message/src/main/java/com/xqbase/baiji/m2/tcp/TcpRequest.java
@@ -0,0 +1,21 @@
+package com.xqbase.baiji.m2.tcp;
+
+import com.xqbase.baiji.m2.Request;
+
+import java.net.URI;
+import java.nio.ByteBuffer;
+
+/**
+ * Created by tonyhe on 15-5-26.
+ */
+public class TcpRequest implements Request {
+    @Override
+    public URI getUri() {
+        return null;
+    }
+
+    @Override
+    public ByteBuffer getEntity() {
+        return null;
+    }
+}
