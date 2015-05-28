61298fd6a6faa94ec97e6491450868945435476b
TonyAdo <coolhzb@163.com>
2015-05-28 18:55:22 +0800

    update the ChannelPoolHandler...

--- baiji-rpc-transport/src/main/java/com/xqbase/baiji/transport/bridge/client/ChannelPoolHandler.java
+++ baiji-rpc-transport/src/main/java/com/xqbase/baiji/transport/bridge/client/ChannelPoolHandler.java
@@ -1,7 +1,21 @@
 package com.xqbase.baiji.transport.bridge.client;
 
+import com.xqbase.baiji.transport.apool.AsyncPool;
+
+import java.nio.channels.Channel;
+
 /**
- * Created by tonyhe on 15-5-27.
+ * Listens for upstream events affecting the state of the channel as
+ * it relates to the pool. This handler does not call super because
+ * it expects to be the last handler in the pipeline, to ensure that
+ * every other handler has had a chance to process the event and finish
+ * with the channel.
+ *
+ * Basically, the handler's job is to return the channel to the pool,
+ * or ask the pool to dispose of the channel, after the response is
+ * received or after an error occurs.
+ *
+ * @author Tony He
  */
-public class ChannelPoolHandler {
+public class ChannelPoolHandler extends UpStreamHandlerWithAttachment<AsyncPool<Channel>> {
 }
