34552b333cbfc4f938a71e6f42e792dd75fcd607
TonyAdo <coolhzb@163.com>
2015-06-10 13:06:27 +0800

    Add the JsonGrammarGenerator...

--- baiji-core/src/main/java/com/xqbase/baiji/io/parsing/JsonGrammarGenerator.java
+++ baiji-core/src/main/java/com/xqbase/baiji/io/parsing/JsonGrammarGenerator.java
@@ -1,8 +1,39 @@
 package com.xqbase.baiji.io.parsing;
 
+import com.xqbase.baiji.schema.Schema;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * The class that generates a grammar suitable to parse Baiji data
  * in JSON format.
  */
 public class JsonGrammarGenerator extends ValidatingGrammarGenerator {
+
+    /**
+     * Returns the non-terminal that is the start symbol
+     * for the grammar for the grammar for the given schema <tt>sc</tt>.
+     */
+    @Override
+    public Symbol generate(Schema schema) {
+        return Symbol.root(generate(schema, new HashMap<LitS, Symbol>()));
+    }
+
+    @Override
+    public Symbol generate(Schema sc, Map<LitS, Symbol> seen) {
+        switch (sc.getType()) {
+            case NULL:
+            case BOOLEAN:
+            case INT:
+            case LONG:
+            case FLOAT:
+            case DOUBLE:
+            case STRING:
+            case BYTES:
+            case UNION:
+                return super.generate(sc, seen);
+        }
+        return super.generate(sc, seen);
+    }
 }
