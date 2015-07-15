cb5c3acbb5bef60b237bd8aac122254ec1424b68
Tony <coolhzb@163.com>
2015-07-16 00:18:01 +0800

    Finish the ServerStats...

--- baiji-rpc-loadbalancer/src/main/java/com/xqbase/baiji/loadbalancer/stats/ServerStats.java
+++ baiji-rpc-loadbalancer/src/main/java/com/xqbase/baiji/loadbalancer/stats/ServerStats.java
@@ -4,6 +4,7 @@ import com.netflix.config.DynamicIntProperty;
 import com.netflix.config.DynamicPropertyFactory;
 import com.xqbase.baiji.loadbalancer.Server;
 
+import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -33,6 +34,12 @@ public class ServerStats {
     private AtomicInteger successiveConnectionFailureCount = new AtomicInteger(0);
     private AtomicInteger activeRequestsCount = new AtomicInteger(0);
 
+    private volatile long lastConnectionFailedTimestamp;
+    private volatile long lastActiveRequestsCountChangeTimestamp;
+    private AtomicLong totalCircuitBreakerBlackOutPeriod = new AtomicLong(0);
+    private volatile long lastAccessedTimestamp;
+    private volatile long firstConnectionTimestamp = 0;
+
     public ServerStats() {
         connectionFailureThreshold = DynamicPropertyFactory.getInstance().getIntProperty(
                 "baiji.loadbalancer.default.conntion.failure.threshold", 3);
@@ -45,6 +52,12 @@ public class ServerStats {
             publisher.start();
         }
         this.server = server;
+    }
+
+    public void close() {
+        if (publisher != null) {
+            publisher.stop();
+        }
     }
 
     private int getBufferSize() {
@@ -92,5 +105,159 @@ public class ServerStats {
             p[i] = percents[i].getValue();
         }
         return p;
+    }
+
+    /**
+     * Call this method to note the response time after every request
+     */
+    public void noteResponseTime(double msc){
+        dataDist.noteValue(msc);
+        responseTimeDist.noteValue(msc);
+    }
+
+    public void incrementActiveRequestsCount() {
+        activeRequestsCount.incrementAndGet();
+        long currentTime = System.currentTimeMillis();
+        lastActiveRequestsCountChangeTimestamp = currentTime;
+        lastAccessedTimestamp = currentTime;
+        if (firstConnectionTimestamp == 0) {
+            firstConnectionTimestamp = currentTime;
+        }
+    }
+
+    public void decrementActiveRequestsCount() {
+        if (activeRequestsCount.decrementAndGet() < 0) {
+            activeRequestsCount.set(0);
+        }
+        lastActiveRequestsCountChangeTimestamp = System.currentTimeMillis();
+    }
+
+    /**
+     * Gets the average total amount of time to handle a request, in milliseconds.
+     */
+    public double getResponseTimeAvg() {
+        return responseTimeDist.getMean();
+    }
+
+    /**
+     * Gets the maximum amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTimeMax() {
+        return responseTimeDist.getMaximum();
+    }
+
+    /**
+     * Gets the minimum amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTimeMin() {
+        return responseTimeDist.getMinimum();
+    }
+
+    /**
+     * Gets the standard deviation in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTimeStdDev() {
+        return responseTimeDist.getStdDev();
+    }
+
+    /**
+     * Gets the average total amount of time to handle a request
+     * in the recent time-slice, in milliseconds.
+     */
+    public double getResponseTimeAvgRecent() {
+        return dataDist.getMean();
+    }
+
+    /**
+     * Gets the 10-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime10thPercentile() {
+        return getResponseTimePercentile(Percent.TEN);
+    }
+
+    /**
+     * Gets the 25-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime25thPercentile() {
+        return getResponseTimePercentile(Percent.TWENTY_FIVE);
+    }
+
+    /**
+     * Gets the 50-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime50thPercentile() {
+        return getResponseTimePercentile(Percent.FIFTY);
+    }
+
+    /**
+     * Gets the 75-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime75thPercentile() {
+        return getResponseTimePercentile(Percent.SEVENTY_FIVE);
+    }
+
+    /**
+     * Gets the 90-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime90thPercentile() {
+        return getResponseTimePercentile(Percent.NINETY);
+    }
+
+    /**
+     * Gets the 95-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime95thPercentile() {
+        return getResponseTimePercentile(Percent.NINETY_FIVE);
+    }
+
+    /**
+     * Gets the 98-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime98thPercentile() {
+        return getResponseTimePercentile(Percent.NINETY_EIGHT);
+    }
+
+    /**
+     * Gets the 99-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime99thPercentile() {
+        return getResponseTimePercentile(Percent.NINETY_NINE);
+    }
+
+    /**
+     * Gets the 99.5-th percentile in the total amount of time spent handling a request, in milliseconds.
+     */
+    public double getResponseTime99point5thPercentile() {
+        return getResponseTimePercentile(Percent.NINETY_NINE_POINT_FIVE);
+    }
+
+    public long getTotalRequestsCount() {
+        return totalRequests.get();
+    }
+
+    private double getResponseTimePercentile(Percent p) {
+        return dataDist.getPercentiles()[p.ordinal()];
+    }
+
+    public String toString(){
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("[Server:" + server + ";");
+        sb.append("\tTotal Requests:" + totalRequests + ";");
+        sb.append("\tTotal blackout seconds:" + totalCircuitBreakerBlackOutPeriod.get() / 1000 + ";");
+        sb.append("\tLast connection made:" + new Date(lastAccessedTimestamp) + ";");
+        if (lastConnectionFailedTimestamp > 0) {
+            sb.append("\tLast connection failure: " + new Date(lastConnectionFailedTimestamp)  + ";");
+        }
+        sb.append("\tFirst connection made: " + new Date(firstConnectionTimestamp)  + ";");
+        sb.append("\taverage resp time:" + getResponseTimeAvg()  + ";");
+        sb.append("\t90 percentile resp time:" + getResponseTime90thPercentile()  + ";");
+        sb.append("\t95 percentile resp time:" + getResponseTime95thPercentile()  + ";");
+        sb.append("\tmin resp time:" + getResponseTimeMin()  + ";");
+        sb.append("\tmax resp time:" + getResponseTimeMax()  + ";");
+        sb.append("\tstddev resp time:" + getResponseTimeStdDev());
+        sb.append("]\n");
+
+        return sb.toString();
     }
 }
