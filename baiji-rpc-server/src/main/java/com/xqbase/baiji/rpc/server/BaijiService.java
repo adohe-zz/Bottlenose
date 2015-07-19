3a1d59c35c63342d856aae9375145bcae7f39fe3
Tony <coolhzb@163.com>
2015-07-20 00:18:42 +0800

    Add the BaijiService class...

---
+++ baiji-rpc-server/src/main/java/com/xqbase/baiji/rpc/server/BaijiService.java
@@ -0,0 +1,28 @@
+package com.xqbase.baiji.rpc.server;
+
+/**
+ * Classes that can be used to bootstrap and launch a Baiji service
+ * from a Java main method.
+ *
+ * @author Tony He
+ */
+public class BaijiService {
+
+    private Class<?> mainServiceClass;
+
+    private BaijiService() {
+
+    }
+
+    private void run(String... args) {
+
+    }
+
+    public static void run(Object source, String... args) {
+
+    }
+
+    public static void run(Object[] sources, String... args) {
+
+    }
+}
