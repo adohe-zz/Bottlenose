e1318a457b2da000673486ebfc90beda4c10ea2b
TonyAdo <coolhzb@163.com>
2015-07-24 18:33:47 +0800

    Add the service host and context......

---
+++ baiji-rpc-server/src/main/java/com/xqbase/baiji/rpc/server/BaijiServiceContext.java
@@ -0,0 +1,9 @@
+package com.xqbase.baiji.rpc.server;
+
+/**
+ * Service Context maintains configuration for per service.
+ *
+ * @author Tony He
+ */
+public class BaijiServiceContext {
+}
---
+++ baiji-rpc-server/src/main/java/com/xqbase/baiji/rpc/server/ServiceHost.java
@@ -0,0 +1,7 @@
+package com.xqbase.baiji.rpc.server;
+
+/**
+ * Created by tonyhe on 15-7-24.
+ */
+public interface ServiceHost {
+}
