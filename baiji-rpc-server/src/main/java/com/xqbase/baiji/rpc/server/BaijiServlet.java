4052b775a153d8381d100e679b8933914d756971
TonyAdo <coolhzb@163.com>
2015-08-13 18:53:39 +0800

    Remove useless servlet.......

--- baiji-rpc-server/src/main/java/com/xqbase/baiji/rpc/server/BaijiServlet.java
+++
@@ -1,9 +0,0 @@
-package com.xqbase.baiji.rpc.server;
-
-/**
- * An {@link javax.servlet.http.HttpServlet} that responds to Baiji RPC requests.
- *
- * @author Tony He
- */
-public class BaijiServlet extends BaijiServletBase {
-}
---
+++ baiji-rpc-server/src/main/java/com/xqbase/baiji/rpc/server/tomcat/BaijiBanner.java
@@ -0,0 +1,49 @@
+package com.xqbase.baiji.rpc.server.tomcat;
+
+import java.io.PrintStream;
+
+import static com.xqbase.baiji.rpc.server.tomcat.AnsiElement.GREEN;
+import static com.xqbase.baiji.rpc.server.tomcat.AnsiElement.DEFAULT;
+import static com.xqbase.baiji.rpc.server.tomcat.AnsiElement.FAINT;
+
+/**
+ * Default Banner implementation which writes the 'Baiji' banner.
+ *
+ * @author Tony He
+ */
+public class BaijiBanner {
+
+    private static final String[] BANNER = { "",
+            "  .   ____                    _       _ _ _",
+            " /\\\\ |    \\             _    (_)  _   \\ \\ \\ \\",
+            "( ( )|___ /   ____     (_)   | | (_)   \\ \\ \\ \\",
+            " \\\\/ |    \\ //    \\\\   | |   | | | |    ) ) ) )",
+            "  .  |____/ \\\\ __ //\\_ |_|   | | |_|   / / / /",
+            " ========================\\\\_/_/ ======/_/_/_/" };
+
+
+    private static final String SPRING_BOOT = " :: Baiji :: ";
+
+    private static final int STRAP_LINE_SIZE = 42;
+
+    public void printBanner(PrintStream printStream) {
+        for (String line : BANNER) {
+            printStream.println(line);
+        }
+        String version = "(v 1.6.0.RELEASE) ^_^ Tony made";
+        String padding = "";
+        while (padding.length() < STRAP_LINE_SIZE
+                - (version.length() + SPRING_BOOT.length())) {
+            padding += " ";
+        }
+
+        printStream.println(AnsiOutput.toString(GREEN, SPRING_BOOT, DEFAULT, padding,
+                FAINT, version));
+        printStream.println();
+    }
+
+    public static void main(String[] args) {
+        BaijiBanner baijiBanner = new BaijiBanner();
+        baijiBanner.printBanner(System.out);
+    }
+}
