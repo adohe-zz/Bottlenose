0c66ca005615f40ddd24315da008a0056192693e
TonyAdo <coolhzb@163.com>
2015-08-20 18:57:47 +0800

    Field overwrite equals/hashcode methods......

--- bn-core/src/main/java/com/xqbase/bn/schema/Field.java
+++ bn-core/src/main/java/com/xqbase/bn/schema/Field.java
@@ -1,6 +1,7 @@
 package com.xqbase.bn.schema;
 
 import com.xqbase.bn.common.util.StringUtils;
+import com.xqbase.bn.util.ObjectUtil;
 import org.codehaus.jackson.JsonGenerator;
 import org.codehaus.jackson.JsonNode;
 
@@ -100,6 +101,39 @@ public class Field {
     }
 
     public JsonNode getDefaultValue() { return defaultValue; }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) {
+            return true;
+        }
+        if (!(obj instanceof Field)) {
+            return false;
+        }
+        Field that = (Field) obj;
+        return ObjectUtil.equals(that.name, name) && that.pos == pos &&
+                ObjectUtil.equals(that.doc, doc)
+                && ObjectUtil.equals(that.ordering, ordering) &&
+                defaultValueEquals(that.defaultValue)
+                && that.schema.equals(schema) && ObjectUtil.equals(that.props, props);
+    }
+
+    private boolean defaultValueEquals(JsonNode thatDefaultValue) {
+        if (null == defaultValue) {
+            return null == thatDefaultValue;
+        }
+        if (Double.isNaN(defaultValue.getDoubleValue())) {
+            return Double.isNaN(thatDefaultValue.getDoubleValue());
+        }
+        return defaultValue.equals(thatDefaultValue);
+    }
+
+    @Override
+    public int hashCode() {
+        return 17 * name.hashCode() + pos + 19 * ObjectUtil.hashCode(doc) +
+                23 * ObjectUtil.hashCode(ordering) + 29 * ObjectUtil.hashCode(defaultValue) +
+                31 * schema.hashCode() + 37 * ObjectUtil.hashCode(props);
+    }
 
     /**
      * Write field in JSON Format.
